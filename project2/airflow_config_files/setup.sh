5da7cea34c651debec9cc413a4cf3305425b46dc
Omer-alt <wafomer912@gmail.com>
2024-10-03 23:24:13 +0000

    Feat: Drift logic and Dag started logic

---
+++ project2/airflow_config_files/setup.sh
@@ -0,0 +1,3 @@
+# source .venv/bin/activate
+export AIRFLOW_HOME=${PWD}/airflow
+airflow users create --username admin --firstname Fotso --lastname Omer --role Admin --email wafomer912@gmail.com --password adminpass
---
+++ project2/webserver.sh
@@ -0,0 +1,5 @@
+
+# Launch webserver
+# source .venv/bin/activate
+export AIRFLOW_HOME=${PWD}/airflow
+airflow webserver --port 8080  # http://localhost:8080
